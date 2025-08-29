dfcd9012b8fa5fd91983902a13e1b7963a5e9664
abc <abc@gmail.com>
2025-08-30 00:12:47 +0530

    Skill updated

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -27,7 +27,7 @@ export default function Header(){
                 {/* <img src={my_img} alt="" width='120px'/> */}
                     <h2>I'm Santhosh T</h2>
                     <h3>Front-end developer</h3>
-                    <h4>A passionate web developer with the skills HTML, CSS, Bootstrap, JavaScript, React JS and Wordpress</h4>
+                    <h4>A passionate web developer with the skills HTML, CSS, Bootstrap, Tailwind CSS, JavaScript, and React JS</h4>
                     <button className='connect btn'><a href="https://wa.me/+918344571951" target='_blank'>Connect with me</a></button>
                     <button className='resume btn' onClick={downloadResume}>Download Resume</button>
                     <div className="social-icons">
